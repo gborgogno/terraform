e025dff705a479742dccfa68c4985250810541f9
gborgogno <geduardae@gmail.com>
2026-02-16 17:14:50 -0300

    Refactor Terraform configurations: update .gitignore, remove unused files, and enhance argocd module with ingress settings

--- live/dev/argocd.tf
+++ live/dev/argocd.tf
@@ -1,19 +1,40 @@
 module "argocd" {
-  source = "../../../terraform-modules/argocd"
+    source = "../../../terraform-modules/argocd"
 
-  # ensure cluster is created before argocd install
-  depends_on = [module.cluster]
+    # ensure cluster is created before argocd install
+    depends_on = [module.cluster]
 
-  release_name  = "argocd"
-  namespace     = "argocd"
-  chart_version = "9.4.2"
-  repository_url = "https://argoproj.github.io/argo-helm"
+    release_name  = "argocd"
+    namespace     = "argocd"
+    chart_version = "9.4.2"
+    repository_url = "https://argoproj.github.io/argo-helm"
 
-  values = {
-    server = {
-      service = {
-        type = "ClusterIP"
-      }
+    values = {
+        server = {
+            service = {
+                type = "ClusterIP"
+            }
+            ingress = {
+                enabled = true
+                hosts = [
+                    {
+                        host = "argocd.devops-sc.com"
+                        paths = [
+                            {
+                                path = "/"
+                                pathType = "Prefix"
+                            }
+                        ]
+                    }
+                ]
+                ingressClassName = "nginx"
+                tls = [
+                    {
+                        secretName = "argocd-tls"
+                        hosts = ["argocd.devops-sc.com"]
+                    }
+                ]
+            }
+        }
     }
-  }
 }
