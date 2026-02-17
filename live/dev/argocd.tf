0aaf3b23f06aab5384c78ad1faa90069aade0cdd
gborgogno <geduardae@gmail.com>
2026-02-17 13:24:04 -0300

    Fix argocd module source URL and clean up ingress configuration

--- live/dev/argocd.tf
+++ live/dev/argocd.tf
@@ -1,10 +1,9 @@
 module "argocd" {
-    source = "git::https://github.com/Devops-Solutions-SC-LTDA/terraform-modules//argocd?ref=main"
+    source = "git::https://github.com/Devops-Solutions-SC/terraform-modules//argocd?ref=main"
 
-
-    release_name  = "argocd"
-    namespace     = "argocd"
-    chart_version = "9.4.2"
+    release_name   = "argocd"
+    namespace      = "argocd"
+    chart_version  = "9.4.2" # Verifique se esta versão é do chart ou da app, a atual do chart é 7.x
     repository_url = "https://argoproj.github.io/argo-helm"
 
     values = {
@@ -13,25 +12,27 @@ module "argocd" {
                 type = "ClusterIP"
             }
             ingress = {
-                enabled = true
-                hosts = [
-                    {
-                        host = "argocd.devops-sc.com"
-                        paths = [
-                            {
-                                path = "/"
-                                pathType = "Prefix"
-                            }
-                        ]
-                    }
-                ]
+                enabled          = true
                 ingressClassName = "nginx"
+                # No Argo CD, hosts é uma lista de strings simples
+                hosts            = ["argocd.devops-sc.com"]
+                paths            = ["/"]
+                pathType         = "Prefix"
+                
+                # Configuração de TLS
                 tls = [
                     {
                         secretName = "argocd-tls"
-                        hosts = ["argocd.devops-sc.com"]
+                        hosts      = ["argocd.devops-sc.com"]
                     }
                 ]
+                
+                # Opcional: Anotações úteis para NGINX
+                annotations = {
+                    "kubernetes.io/ingress.class"                    = "nginx"
+                    "nginx.ingress.kubernetes.io/ssl-passthrough"    = "true"
+                    "nginx.ingress.kubernetes.io/backend-protocol"   = "HTTPS"
+                }
             }
         }
     }
